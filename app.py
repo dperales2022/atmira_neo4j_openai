9403bd7e13e3b948f90a393bfd0e7e0d3b5ea0dd
dperales2022 <111503354+dperales2022@users.noreply.github.com>
2024-04-28 12:32:19 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,209 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+import json
+from neo4j import GraphDatabase
+import requests
+from tempfile import NamedTemporaryFile
+from langchain_community.graphs import Neo4jGraph
+from langchain_community.graphs.graph_document import (
+    Node as BaseNode,
+    Relationship as BaseRelationship,
+    GraphDocument,
+)
+from langchain.schema import Document
+from typing import List, Dict, Any, Optional
+from langchain.pydantic_v1 import Field, BaseModel
+from langchain.chains.openai_functions import (
+    create_openai_fn_chain,
+    create_structured_output_chain,
+)
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import TokenTextSplitter
+from tqdm import tqdm
+
+import nest_asyncio
+nest_asyncio.apply()
+
+# Load environment variables
+load_dotenv()
+
+# FastAPI application
+app = FastAPI()
+
+# Add a CORS middleware to the application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+# Neo4j and OpenAI configurations
+NEO4J_URL = os.environ["NEO4J_URI"]
+NEO4J_USER = os.environ["NEO4J_USERNAME"]
+NEO4J_PASSWORD = os.environ["NEO4J_PASSWORD"]
+NEO4J_DATABASE = "neo4j"
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
+EMBEDDING_MODEL = "text-embedding-3-small"
+GENERATION_MODEL = "gpt-4-0125-preview"
+TABLE_REF_SUFFIX = '_table_ref'
+TABLE_ID_SUFFIX  = '_table'
+
+class Property(BaseModel):
+  """A single property consisting of key and value"""
+  key: str = Field(..., description="key")
+  value: str = Field(..., description="value")
+
+class Node(BaseNode):
+    properties: Optional[List[Property]] = Field(
+        None, description="List of node properties")
+
+class Relationship(BaseRelationship):
+    properties: Optional[List[Property]] = Field(
+        None, description="List of relationship properties"
+    )
+
+class KnowledgeGraph(BaseModel):
+    """Generate a knowledge graph with entities and relationships."""
+    nodes: List[Node] = Field(
+        ..., description="List of nodes in the knowledge graph")
+    rels: List[Relationship] = Field(
+        ..., description="List of relationships in the knowledge graph"
+    )
+
+def format_property_key(s: str) -> str:
+    words = s.split()
+    if not words:
+        return s
+    first_word = words[0].lower()
+    capitalized_words = [word.capitalize() for word in words[1:]]
+    return "".join([first_word] + capitalized_words)
+
+def props_to_dict(props) -> dict:
+    """Convert properties to a dictionary."""
+    properties = {}
+    if not props:
+      return properties
+    for p in props:
+        properties[format_property_key(p.key)] = p.value
+    return properties
+
+def map_to_base_node(node: Node) -> BaseNode:
+    """Map the KnowledgeGraph Node to the base Node."""
+    properties = props_to_dict(node.properties) if node.properties else {}
+    # Add name property for better Cypher statement generation
+    properties["name"] = node.id.title()
+    return BaseNode(
+        id=node.id.title(), type=node.type.capitalize(), properties=properties
+    )
+
+def map_to_base_relationship(rel: Relationship) -> BaseRelationship:
+    """Map the KnowledgeGraph Relationship to the base Relationship."""
+    source = map_to_base_node(rel.source)
+    target = map_to_base_node(rel.target)
+    properties = props_to_dict(rel.properties) if rel.properties else {}
+    return BaseRelationship(
+        source=source, target=target, type=rel.type, properties=properties
+    )
+
+def get_extraction_chain(
+    allowed_nodes: Optional[List[str]] = None,
+    allowed_rels: Optional[List[str]] = None
+    ):
+    prompt = ChatPromptTemplate.from_messages(
+        [(
+          "system",
+          f"""# Knowledge Graph Instructions for GPT-4
+## 1. Overview
+You are a top-tier algorithm designed for extracting information in structured formats to build a knowledge graph.
+- **Nodes** represent entities and concepts. They're akin to Wikipedia nodes.
+- The aim is to achieve simplicity and clarity in the knowledge graph, making it accessible for a vast audience.
+## 2. Labeling Nodes
+- **Consistency**: Ensure you use basic or elementary types for node labels.
+  - For example, when you identify an entity representing a person, always label it as **"person"**. Avoid using more specific terms like "mathematician" or "scientist".
+- **Node IDs**: Never utilize integers as node IDs. Node IDs should be names or human-readable identifiers found in the text.
+{'- **Allowed Node Labels:**' + ", ".join(allowed_nodes) if allowed_nodes else ""}
+{'- **Allowed Relationship Types**:' + ", ".join(allowed_rels) if allowed_rels else ""}
+## 3. Handling Numerical Data and Dates
+- Numerical data, like age or other related information, should be incorporated as attributes or properties of the respective nodes.
+- **No Separate Nodes for Dates/Numbers**: Do not create separate nodes for dates or numerical values. Always attach them as attributes or properties of nodes.
+- **Property Format**: Properties must be in a key-value format.
+- **Quotation Marks**: Never use escaped single or double quotes within property values.
+- **Naming Convention**: Use camelCase for property keys, e.g., `birthDate`.
+## 4. Coreference Resolution
+- **Maintain Entity Consistency**: When extracting entities, it's vital to ensure consistency.
+If an entity, such as "John Doe", is mentioned multiple times in the text but is referred to by different names or pronouns (e.g., "Joe", "he"),
+always use the most complete identifier for that entity throughout the knowledge graph. In this example, use "John Doe" as the entity ID.
+Remember, the knowledge graph should be coherent and easily understandable, so maintaining consistency in entity references is crucial.
+## 5. Strict Compliance
+Adhere to the rules strictly. Non-compliance will result in termination.
+          """),
+            ("human", "Use the given format to extract information from the following input: {input}"),
+            ("human", "Tip: Make sure to answer in the correct format"),
+        ])
+    return create_structured_output_chain(KnowledgeGraph, llm, prompt, verbose=False)
+
+def extract_and_store_graph(
+    document: Document,
+    nodes:Optional[List[str]] = None,
+    rels:Optional[List[str]]=None) -> None:
+    # Extract graph data using OpenAI functions
+    extract_chain = get_extraction_chain(nodes, rels)
+    data = extract_chain.invoke(document.page_content)['function']
+    # Construct a graph document
+    graph_document = GraphDocument(
+      nodes = [map_to_base_node(node) for node in data.nodes],
+      relationships = [map_to_base_relationship(rel) for rel in data.rels],
+      source = document
+    )
+    # Store information into a graph
+    graph.add_graph_documents([graph_document])
+
+def download_pdf(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Raises an HTTPError for bad responses
+
+    # Create a temporary file to hold the downloaded PDF
+    with NamedTemporaryFile(delete=False, suffix='.pdf') as tmp_file:
+        tmp_file.write(response.content)
+        return tmp_file.name  # Return the file path of the downloaded PDF
+
+# Pydantic model for receiving PDF file path and customer_id
+class ProcessRequest(BaseModel):
+    pdf_file_path: str
+    customer_id: str
+    customer_name: str
+
+@app.post("/process-pdf/")
+async def process_pdf(request: ProcessRequest):
+    try:
+        graph = Neo4jGraph(
+            url=NEO4J_URL,
+            username=NEO4J_USER,
+            password=NEO4J_PASSWORD
+        )       
+        
+        llm = ChatOpenAI(model="gpt-3.5-turbo-16k", temperature=0)
+
+        # Load and parse PDF
+        pdf_file_path = download_pdf(request.pdf_file_path)
+        text_splitter = TokenTextSplitter(chunk_size=2048, chunk_overlap=24)
+        documents = text_splitter.split_documents(pdf_file_path)
+
+        for i, d in tqdm(enumerate(documents), total=len(documents)):
+            extract_and_store_graph(d)
+        
+        return {"status": "success", "message": "PDF processed and data stored successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Run locally for development
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, loop='asyncio')
